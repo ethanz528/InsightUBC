8bd64f6237526aab4e4ea47cc2a66a52c103432a
goldak47 <namdar.shafiei.1997@gmail.com>
2021-02-23 12:50:24 -0800

    test

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -18,6 +18,7 @@ export default class InsightFacade implements IInsightFacade {
         Log.trace("InsightFacadeImpl::init()");
     }
 
+    // test submit
     public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
         let newDataset: Dataset;
         if (isIdInvalid(id)) {
